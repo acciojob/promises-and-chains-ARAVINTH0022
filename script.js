a294323b6a17f63c4e4aed5d445ed75f8b4e13a0
ARAVINTH0022 <113502178+ARAVINTH0022@users.noreply.github.com>
2026-08-10 06:50:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //your JS code here. If required.
+const form = document.getElementById("voteForm");
+const ageInput = document.getElementById("age");
+const nameInput = document.getElementById("name");
+
+function checkVotingAge(name, age) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (age > 18) {
+        resolve(`Welcome, ${name}. You can vote.`);
+      } else {
+        reject(`Oh sorry ${name}. You aren't old enough.`);
+      }
+    }, 4000);
+  });
+}
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const name = nameInput.value.trim();
+  const age = ageInput.value.trim();
+
+  if (name === "" || age === "") {
+    alert("Please enter valid details.");
+    return;
+  }
+
+  checkVotingAge(name, Number(age))
+    .then((message) => {
+      alert(message);
+    })
+    .catch((message) => {
+      alert(message);
+    });
+});
